59247e7663a02fee916a5cdf37a552fb58d1051b
romandob <roman.dobrynin@hebsdigital.com>
2016-08-10 12:43:25 +0300

    Added Object canvas and game

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,60 @@
 //Canvas
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+var canvas = new Object();
+canvas.element = document.getElementById('canvas');
+canvas.context = canvas.element.getContext('2d');
+canvas.width = canvas.element.getAttribute('width');
+canvas.height = canvas.element.getAttribute('height');
+canvas.cellWidth = 10;
 
+var ctx = canvas.context;
 var width = canvas.width;
 var height = canvas.height;
 
-var cw = 10;
+var cw = canvas.cellWidth;
 var widthInELEMENTs = width / cw;
 var heightInELEMENTs = height / cw;
-var score = 0;
-var speed = 1;
 
-var drawBorder = function () {
-    ctx.fillStyle = "Black";
-    ctx.fillRect(0, 0, width, cw);
-    ctx.fillRect(0, height - cw, width, cw);
-    ctx.fillRect(0, 0, cw, height);
-    ctx.fillRect(width - cw, 0, cw, height);
+canvas.redraw = function(){
+    this.draw(0, 0, '#99cc99', 'black', this.width, this.height);
 };
 
-var setScore = function () {
-
+canvas.draw = function(x, y, fillCol, strokeColour, width, height) {
+    this.context.fillStyle = fillCol;
+    this.context.fillRect(x*canvas.cellWidth, y*canvas.cellWidth, width, height);
+    this.context.strokeStyle = strokeColour;
+    this.context.strokeRect(x*canvas.cellWidth, y*canvas.cellWidth, width, height);
+    this.context.fillStyle = "Black";
+    this.context.fillRect(0, 0, width, canvas.cellWidth);
+    this.context.fillRect(0, height - canvas.cellWidth, width, canvas.cellWidth);
+    this.context.fillRect(0, 0, canvas.cellWidth, height);
+    this.context.fillRect(width - canvas.cellWidth, 0, canvas.cellWidth, height);
 };
 
 
-var gameOver = function () {
+var game = new Object();
+game.score = 0;
+game.speed = 1;
+game.frequency = 20;
+game.level = "worm";
+game.setScore = function() {
+    document.getElementById('score').innerHTML = this.score;
+};
+game.updateLevel = function() {
+    if(game.speed == 3) {
+        game.level = "Speed Worm";
+    }
+    else if(game.speed > 3) {
+        game.level = "Mega Worm";
+    }
+    document.getElementById('level').innerHTML = this.level;
+};
 
+canvas.drawBorder = function () {
+    canvas.context.fillStyle = "Black";
+    canvas.context.fillRect(0, 0, width, cw);
+    canvas.context.fillRect(0, height - cw, width, cw);
+    canvas.context.fillRect(0, 0, cw, height);
+    canvas.context.fillRect(width - cw, 0, cw, height);
 };
 
 
@@ -34,28 +63,21 @@ var ELEMENT = function (col, row) {
     this.row = row;
 };
 
-ELEMENT.prototype.drawSquare = function(color) {
+ELEMENT.prototype.drawSquare = function(color, border) {
     var x = this.col * cw;
     var y = this.row * cw;
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, cw, cw);
+    canvas.context.fillStyle = color;
+    canvas.context.fillRect(x, y, cw, cw);
+    canvas.context.strokeStyle = border;
+    canvas.context.strokeRect(x, y, cw, cw)
 };
-
-ELEMENT.prototype.drawSnake = function(color, border) {
-    var x = this.col * cw;
-    var y = this.row * cw;
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, cw, cw);
-    ctx.strokeStyle = border;
-    ctx.strokeRect(x, y, cw, cw)
-};
-
 
 ELEMENT.prototype.equal = function(otherELEMENT) {
     return this.col === otherELEMENT.col && this.row === otherELEMENT.row;
 };
 
 var Snake = function () {
+    // draw snake
     this.segments = [
         new ELEMENT(7, 5),
         new ELEMENT(6, 5),
@@ -64,21 +86,20 @@ var Snake = function () {
         new ELEMENT(3, 5)
     ];
     this.direction = "right";
-    this.nextDirection = "right";
+    this.nextDirection = "down";
 };
 
 
 Snake.prototype.draw = function() {
-
     for(var i = 0; i < this.segments.length; i++){
-        this.segments[i].drawSnake("black", "white");
-
+        this.segments[i].drawSquare("black", "#99cc99");
     }
 };
 
-Snake.prototype.speed = function() {
 
-};
+
+
+// todo
 
 Snake.prototype.move = function() {
 
